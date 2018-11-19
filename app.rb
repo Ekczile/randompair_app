c471cc4f790a7e309d2c8bd6fb6a1cd40b63bf8a
Steven <ekczile@gmail.com>
2018-11-19 16:17:22 -0500

    new page that changes grouping

--- app.rb
+++ app.rb
@@ -15,12 +15,37 @@ post '/home' do
             redirect '/'
         end
     end
-    redirect 'pairs?names=' + names
+    redirect 'changedpairs?names=' + names
 end     
 
+get '/changedpairs' do
+    names = params[:names].split(",")
+    pairednames = create_random_pairs(names)
+    names = names.join(",")
+    erb :changedpairs, locals: {names: names, pairednames: pairednames}
+end
+
+post '/changedpairs' do
+    names = params[:names].split(",")
+    pairednames = create_random_pairs(names)
+    names = names.join(",")
+    redirect 'pairs?narmes=' + names + '&pairednames=' + pairednames
+end
 
 get '/pairs' do
     names = params[:names].split(",")
+    p "this is my params3 #{params}"
     pairednames = create_random_pairs(names)
+    names = names.join(',')
     erb :pairs, locals: {names: names, pairednames: pairednames}
+end
+
+post '/pairs' do
+    names = params[:names].join(",")
+    names = names.split(',')
+    pairednames = create_random_pairs(names)
+    names = names.join(",")
+    pairednames = pairednames.join(",")
+    p "this is names #{names}"
+    redirect 'pairs?names=' + names + '&pairednames=' + pairednames
 end
